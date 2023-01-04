668c994c9da2f82fa0bdeac412825923da61fabe
Ifeanyirochukwu <ifydan73@gmail.com>
2023-01-04 09:32:32 -0800

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * find_square - multiplies two digits
+ * @n: 1st digit in the multiplication
+ * @num: 2nd digit to be multiplied
+ * Description: multiplies two digits
+ * Return: the result of the multiplication
+ */
+int find_square(int n, int num)
+{
+	if (num * num == n)
+		return (num);
+	if (num * num > n)
+		return (-1);
+	return (find_square(n, ++num));
+}
+
+
+/**
+ * _sqrt_recursion - function that returns the natural square root of a number
+ * @n: 1st digit in the multiplication
+ * Description: multiplies two digits
+ * Return: the result of the multiplication
+ */
+int _sqrt_recursion(int n)
+{
+	return (find_square(n, 1));
+}
