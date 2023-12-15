e44fe3fc4982805e74bb82008f1d8500505db938
Ifeanyirochukwu <ifydan73@gmail.com>
2023-12-15 04:51:56 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#include <stdio.h>
+
+/**
+ * main - "Prints the size of various types.
+ * Return: Always 0 if executed properly
+ */
+
+int main(void)
+
+{
+	printf("Size of char: %d byte(s)\n", sizeof(char));
+	printf("Size of int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
